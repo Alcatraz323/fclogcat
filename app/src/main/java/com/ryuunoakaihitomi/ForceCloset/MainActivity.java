66e9e749435031d26252022b15a2709895fdd1a1
Alcatraz323 <Alcatraz32323@gmail.com>
2016-11-06 13:47:31 +0800

    Initial commit

---
+++ app/src/main/java/com/ryuunoakaihitomi/ForceCloset/MainActivity.java
@@ -0,0 +1,14 @@
+package com.ryuunoakaihitomi.ForceCloset;
+
+import android.app.*;
+import android.os.*;
+
+public class MainActivity extends Activity 
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
